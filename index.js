f8258556b30a96e73e40430cb92900ec4eb7a8e1
LetsCodeSomething <90828101+LetsCodeSomething@users.noreply.github.com>
2021-12-01 17:34:02 +1000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+import chalk from 'chalk';
+
+console.log(chalk.green('Created with help of NodeJS, NPM and Chalk.'));
+console.log(chalk.greenBright('████████████████'));
+console.log(chalk.greenBright('████████████████'));
+console.log(chalk.greenBright('██') +
+    chalk.black('████') +
+    chalk.greenBright('████') +
+    chalk.black('████') +
+    chalk.greenBright('██'));
+console.log(chalk.greenBright('██') +
+    chalk.black('████') +
+    chalk.greenBright('████') +
+    chalk.black('████') +
+    chalk.greenBright('██'));
+console.log(chalk.greenBright('██████') +
+    chalk.black('████') +
+    chalk.greenBright('██████'));
+console.log(chalk.greenBright('████') +
+    chalk.black('████████') +
+    chalk.greenBright('████'));
+console.log(chalk.greenBright('████') +
+    chalk.black('████████') +
+    chalk.greenBright('████'));
+console.log(chalk.greenBright('████') +
+    chalk.black('██') +
+    chalk.greenBright('████') +
+    chalk.black('██') +
+    chalk.greenBright('████'));
